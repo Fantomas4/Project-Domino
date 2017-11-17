7d80628bf1ccce83b9856fe30bff9e044a2ff34b
Fantomas4 <stefanokf@csd.auth.gr>
2017-11-17 14:45:49 +0200

    Began work on Hungarian game mode.

--- src/maingame/MainGame.java
+++ src/maingame/MainGame.java
@@ -17,11 +17,24 @@ public class MainGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Solo1 gameInstance = new Solo1();
-//        gameInstance.run();
-          Hungarian gameInstance = new Hungarian(2);
-          gameInstance.run();
-          
+        int userChoice;
+
+        Menu mainMenu = new Menu();
+
+        do {
+            userChoice = mainMenu.menuChoice();
+
+            if (userChoice == 1) {
+                Solo1 gameInstance = new Solo1();
+                gameInstance.run();
+            } else if (userChoice == 2) {
+                Hungarian gameInstance = new Hungarian(2);
+                gameInstance.run();
+            }
+
+        } while (userChoice != 0);
+        
+        System.out.printf("%n%n%n                     ===== Thank you for playing ===== %n%n%n");
 
     }
 
--- src/maingame/Menu.java
+++ src/maingame/Menu.java
@@ -5,20 +5,43 @@
  */
 package maingame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sierra Kilo
  */
 public class Menu {
-    
-    public Menu(){}
-    
-    public void showMenu() {
+
+    public Menu() {
+    }
+
+    public int menuChoice() {
+
+        Scanner input = new Scanner(System.in);
+        int choice;
+        
+        System.out.printf("%n%n%n%n%n%n");
+        System.out.println("                ======== Welcome to the Domino game ========");
+        System.out.printf("%n%n%n");
+        System.out.println("> Select an option: ");
+        System.out.printf("%n");
         System.out.println("0. Exit program.");
         System.out.println("1. Start a new Solo 1 game.");
-        System.out.println("2.Start a new Hungarian game for 2 players.");
-                
+        System.out.println("2. Start a new Hungarian game for 2 players.");
+        System.out.printf("%n%n");
+
+        do {
+            choice = input.nextInt();
+            if (choice < 0 || choice > 2) {
+                System.out.printf("%n%n");
+                System.out.printf("> Wrong choice number. Please choose one of the given options. %n");
+                System.out.printf("> Select an option: ");
+            }
+        } while (choice < 0 || choice > 2);
+
+        return choice;
+
     }
-   
-    
+
 }
